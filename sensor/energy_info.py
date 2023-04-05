e5af8568c77ec89ad701e855c5c6e0fd9d340610
Ray <ray.wright@sas.com>
2023-04-05 13:50:49 -0400

    Completed Module

--- sensor/energy_info.py
+++ sensor/energy_info.py
@@ -1,18 +1,22 @@
 from house_info import HouseInfo
-from datetime import date, datetime
+from datetime import date
+
 
 class EnergyData(HouseInfo):
-    ENERGY_PER_BULB = 0.2
+
+    ENERGY_PER_BULB = 0.2        # in watts
     ENERGY_BITS = 0x0F0
 
-    def _get_energy(self,rec):
-        energy = int(rec, base=16) &self.ENERGY_BITS
-        energy = energy >> 4
+    def _get_energy(self, rec):
+        energy = int(rec, base=16)
+        energy = energy & self.ENERGY_BITS            # mask ENERGY bits
+        energy = energy >> 4                          # shift right
         return energy
-    
+
     def _convert_data(self, data):
         recs = []
         for rec in data:
+            # Convert string of hex into actual integers based 10
             recs.append(self._get_energy(rec))
         return recs
 
