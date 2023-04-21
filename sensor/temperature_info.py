904309b78d25d2a4ce2d5462d867e6919fdcdb61
Ray <ray.wright@sas.com>
2023-04-21 13:27:47 -0400

    done!!

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -5,7 +5,7 @@ from datetime import date
 class TemperatureData(HouseInfo):
 
     def _convert_data(self,data):
-        recs = list()
+        recs = {}
 
         for rec in data:
             recs.append(int(rec, base=10))
