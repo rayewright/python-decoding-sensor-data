b5790bcf4cadee3f2485206f24cb0a47ce92f264
Ray <ray.wright@sas.com>
2023-04-21 13:21:28 -0400

    done

--- sensor/particle_count_info.py
+++ sensor/particle_count_info.py
@@ -18,7 +18,7 @@ class ParticleData(HouseInfo):
         return self._convert_data(recs)
     
     def get_data_concentrations(self, data):
-        particulate = dict(good = 0, moderate=0, bad = 0)
+        particulate = {'good':0, 'moderate':0, 'bad':0}
 
         for rec in data:
 
